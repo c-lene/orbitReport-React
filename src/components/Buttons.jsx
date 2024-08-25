16204b698a7883d256db701073a8d57972e18dab
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-25 16:52:58 -0500

    CT edit - added first buttoon map function, but confused if did the second button correctly per note

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,36 @@
+import satData from "./satData";
+
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => {
+      return (
+        <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+        </button>
+      );
+    })}
+
+    {/* Need to Pass "satData" to "setSat" */}
+    <button onClick={(satData) => setSat}>All Orbits</button>
     </div>
+
   );
+  
+
+  
+  
+  
+  
+  
+  
+  // return (
+  //   <div>
+  //     <button>Placeholder Button</button>
+  //     <button>All Orbits</button>
+  //   </div>
+  // );
 };
 
 export default Buttons;
