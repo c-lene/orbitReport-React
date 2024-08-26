00cef97f2f763207631301cceb5fee4abc57959d
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-08-26 12:55:14 -0500

    CT completed - used mapping to update table, added CSS styling

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,16 +1,34 @@
+import "./styling.css";
+
 const Table = ({ sat }) => {
+  
+  // Using map() function to iterate through each object in array
+  const info = sat.map((data, id) => (
+    <tr key={id}>
+      <td>{data.name}</td>
+      <td>{data.type}</td>
+      <td>{data.launchDate}</td>
+      <td>{data.operational ? "active" : "inactive"}</td>
+    </tr>
+  ));
+
+  
   return (
     <table>
+      
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
+
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+      {info}
      </tbody>
+
    </table>
   );
 };
